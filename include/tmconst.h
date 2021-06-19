a64961ef7ecb41701ccaed8e62daf07db65c1fa2
Ishi Maheshwari <ishimaheshwari122@gmail.com>
2021-06-20 00:12:22 +0530

    Initial commit

---
+++ include/tmconst.h
@@ -0,0 +1,5 @@
+#ifndef __tmconst__
+#define __tmconst__ 238
+#define SUCCESSFUL 1
+#define FAILED 0
+#endif
---
+++ testcases/eg1.c
@@ -0,0 +1,10 @@
+#include<tmstack.h>
+#include<tmstack.h>
+#include<stdio.h>
+int main()
+{
+Stack stack1;
+printf("Great");
+return 0;
+}
+
